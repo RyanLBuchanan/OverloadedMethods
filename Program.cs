9f4097fb5107d9395b3183be8876dac2bc2380f7
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2022-10-06 10:43:38 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿namespace OverloadedMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create arrays of int, double and char
+            int[] intArray = { 1, 2, 3, 4, 5, 6 };
+            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7 };
+            char[] charArray = { 'H', 'E', 'L', 'L', 'O', };
+
+            Console.WriteLine("Array intArray contains:");
+            DisplayArray(intArray);
+            Console.WriteLine("Array doubleArray contains:");
+            DisplayArray(doubleArray);
+            Console.WriteLine("Array charArray contains:");
+            DisplayArray(charArray);
+        }
+
+        // Output int array
+        private static void DisplayArray(int[] inputArray)
+        {
+            foreach (var element in inputArray)
+            {
+                Console.Write($"{element} ");
+            }
+
+            Console.WriteLine();
+        }
+
+        // Output double array
+        private static void DisplayArray(double[] inputArray)
+        {
+            foreach (var element in inputArray)
+            {
+                Console.Write($"{element} ");
+            }
+
+            Console.WriteLine();
+        }
+
+        // Output char array
+        private static void DisplayArray(char[] inputArray)
+        {
+            foreach (var element in inputArray)
+            {
+                Console.Write($"{element} ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
